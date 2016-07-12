50f3e2c956421a833eba14bf649f3553553eb216
penghuili <ichpenghui@gmail.com>
2016-07-13 00:06:35 +0200

    todo view finished, logic not works

--- app_client/app.js
+++ app_client/app.js
@@ -1,1 +1,31 @@
-angular.module("todoCalendar")
+(function () {
+
+  angular.module("todoCalendar", ["ngRoute"]);
+  
+  config.$inject = ["$routeProvider", "$locationProvider"];
+  function config($routeProvider, $locationProvider) {
+    $routeProvider
+      .when("/", {
+        templateUrl: "home/home.view.html",
+        controller: "homeCtrl",
+        controllerAs: "vm"
+      })
+      .when("/todo", {
+        templateUrl: "todo/todo.view.html",
+        controller: "todoCtrl",
+        controllerAs: "vm"
+      })
+      .when("/todo/add", {
+        templateUrl: "todoAdd/todoAdd.view.html",
+        controller: "todoAddCtrl",
+        controllerAs: "vm"
+      })
+      .otherwise({redirectTo: "/"});
+    $locationProvider.html5Mode(true);
+  }
+
+  angular
+    .module("todoCalendar")
+    .config(["$routeProvider", "$locationProvider", config]);
+
+})();
