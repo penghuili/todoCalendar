cd2a697693ca69eac969a5d126fd61cd1df4086d
penghuili <ichpenghui@gmail.com>
2016-07-13 22:59:00 +0200

    now inbox can be checked out, upcoming, today, completed, all still cannot

---
+++ app_client/common/service/utils.js
@@ -0,0 +1,81 @@
+(function() {
+
+  angular
+    .module("todoCalendar")
+    .service("utils", utils);
+
+  function utils() {
+    var addZero = function(num) {
+      if(num < 10) {
+        return "0" + num.toString();
+      }
+      return num.toString();
+    };
+
+    var amOrPm = function(hour) {
+      if(hour > 12) {
+        return [addZero(hour - 12), "PM"];
+      }
+      return [addZero(hour), "AM"];
+    };
+
+    var addSlash = function(date) {
+      var year = date.substring(0,4),
+        month = date.substring(4,6),
+        day = date.substring(6);
+      return month + "/" + day + "/" + year;
+    };
+
+    var parseDate = function(date) {
+      var year = date.getFullYear().toString(),
+        month = addZero(date.getMonth() + 1),
+        day = addZero(date.getDate());
+      return year + month + day;
+    };
+
+    var parseTime = function(time) {
+      var hour = amOrPm(time.getHours()),
+        minutes = addZero(time.getMinutes());
+      return hour[0] + ":" + minutes + " " + hour[1];
+    };
+
+    var getUpcomingArr = function(date, threshold) {
+      var tmp = new Date(date),
+          upArr = [],
+          i = 0;
+      while(i < threshold) {
+        tmp.setDate(tmp.getDate() + 1);
+        upArr.unshift(parseDate(tmp));
+        i++;
+      }
+      return upArr;
+    };
+
+    var getTasks = function(obj, dateArr, completed) {
+      var tasks = [],
+          len = dateArr.length,
+          i = 0,
+          checkCompleted = function(value) {
+            return value.completed === completed;
+          };
+      if(obj.length + 1) {
+        tasks = obj.filter(checkCompleted);
+      } else {
+        for(i = 0; i < len; i++) {
+          obj[dateArr[i]] = obj[dateArr[i]] || [];
+          tasks = tasks.concat(obj[dateArr[i]].filter(checkCompleted));
+        }
+      }
+      return tasks;
+    };
+
+    return {
+      parseDate: parseDate,
+      parseTime: parseTime,
+      getUpcomingArr: getUpcomingArr,
+      getTasks: getTasks,
+      addSlash: addSlash
+    };
+  }
+
+})();
